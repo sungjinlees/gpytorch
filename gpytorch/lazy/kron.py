c0ab78e98540afaea6e66b5a4a9804484ca5bac7
Geoff Pleiss <gpleiss@gmail.com>
2017-06-26 09:50:29 -0400

    Kron takes diagonal add argument

--- gpytorch/lazy/kron.py
+++ gpytorch/lazy/kron.py
@@ -11,7 +11,12 @@ class Kron(LazyFunction):
     variable_class = KronVariable
 
 
-    def forward(self, mat_1, mat_2):
+    def forward(self, mat_1, mat_2, diag):
+        if diag.numel() == 1:
+            self.diag_val = diag.squeeze()[0]
+        else:
+            raise RuntimeError('Diag must be a single-element tensor')
+
         self.save_for_backward(mat_1, mat_2)
 
         m, n = mat_1.size()
@@ -19,26 +24,33 @@ class Kron(LazyFunction):
 
         mat_1_exp = mat_1.unsqueeze(1).unsqueeze(3).expand(m, p, n, q)
         mat_2_exp = mat_2.unsqueeze(0).unsqueeze(2).expand(m, p, n, q)
-        return (mat_1_exp * mat_2_exp).view(m * p, n * q)
+        res = (mat_1_exp * mat_2_exp).view(m * p, n * q)
+        if self.diag_val is not None:
+            res.add_(torch.eye(m * p, n * q).mul_(self.diag_val))
+
+        return res
 
 
     def backward(self, grad_output):
+        grad_mat_1 = None
+        grad_mat_2 = None
+        diag_grad = None
         mat_1, mat_2 = self.saved_tensors
+
+        if self.needs_input_grad[2]:
+            diag_grad = grad_output.new().resize_(1)
+            diag_grad.fill_(grad_output.trace())
 
         m, n = mat_1.size()
         p, q = mat_2.size()
-
         grad_output = grad_output.view(m, p, n, q)
-        grad_mat_1 = None
-        grad_mat_2 = None
 
         if self.needs_input_grad[0]:
             mat_2_exp = mat_2.unsqueeze(0).unsqueeze(2).expand(m, p, n, q)
             grad_mat_1 = torch.mul(mat_2_exp, grad_output).sum(3).sum(1).squeeze(3).squeeze(1)
 
-
         if self.needs_input_grad[1]:
             mat_1_exp = mat_1.unsqueeze(1).unsqueeze(3).expand(m, p, n, q)
             grad_mat_2 = torch.mul(mat_1_exp, grad_output).sum(2).sum(0).squeeze(2).squeeze(0)
 
-        return grad_mat_1, grad_mat_2
+        return grad_mat_1, grad_mat_2, diag_grad
--- test/math/functions/kron_test.py
+++ test/math/functions/kron_test.py
@@ -1,5 +1,7 @@
+import math
 import torch
 from torch.autograd import Function, Variable
+from torch.nn import Parameter
 from gpytorch.lazy import Kron, KronVariable
 
 a = torch.Tensor([
@@ -13,7 +15,7 @@ b = torch.Tensor([
     [40],
 ])
 
-def test_forward():
+def test_forward_without_diag():
     actual = torch.Tensor([
         [ 10,   20,   30],
         [ 20,   40,   60],
@@ -24,14 +26,33 @@ def test_forward():
         [120,  150,  180],
         [160,  200,  240],
     ])
-    output = Kron()(Variable(a), Variable(b))
+    cvar = Parameter(torch.Tensor([0]))
+    output = Kron()(Variable(a), Variable(b), cvar)
     assert(isinstance(output, KronVariable))
 
     res = output.evaluate().data
     assert(torch.norm(res - actual) < 1e-4)
 
 
-def test_backward():
+def test_forward_with_diag():
+    actual = torch.Tensor([
+        [ 13,   20,   30],
+        [ 20,   43,   60],
+        [ 30,   60,   93],
+        [ 40,   80,  120],
+        [ 40,   50,   60],
+        [ 80,  100,  120],
+        [120,  150,  180],
+        [160,  200,  240],
+    ])
+    output = Kron()(Variable(a), Variable(b), Parameter(torch.Tensor([3])))
+    assert(isinstance(output, KronVariable))
+
+    res = output.evaluate().data
+    assert(torch.norm(res - actual) < 1e-4)
+
+
+def test_backward_without_diag():
     gradient = torch.Tensor([
         [ 1,   2,   3],
         [ 4,   5,   6],
@@ -73,9 +94,61 @@ def test_backward():
 
     avar = Variable(a, requires_grad=True)
     bvar = Variable(b, requires_grad=True)
-    outvar = Kron()(avar, bvar).evaluate()
+    cvar = Parameter(torch.Tensor([0]))
+    outvar = Kron()(avar, bvar, cvar).evaluate()
     outvar.backward(gradient=gradient)
 
     assert(torch.norm(avar.grad.data - actual_a) < 1e-6)
     assert(torch.norm(bvar.grad.data - actual_b) < 1e-6)
+
+
+def test_backward_with_diag():
+    gradient = torch.Tensor([
+        [ 1,   2,   3],
+        [ 4,   5,   6],
+        [ 7,   8,   9],
+        [10,  11,  12],
+        [13,  14,  15],
+        [16,  17,  18],
+        [19,  20,  21],
+        [22,  23,  24],
+    ])
+
+    actual_a = 10 * torch.Tensor([
+        [ 1,   2,   3],
+        [13,  14,  15],
+    ]) + 20 * torch.Tensor([
+        [ 4,   5,   6],
+        [16,  17,  18],
+    ]) + 30 * torch.Tensor([
+        [ 7,   8,   9],
+        [19,  20,  21],
+    ]) + 40 * torch.Tensor([
+        [10,  11,  12],
+        [22,  23,  24],
+    ])
+
+    actual_b = torch.Tensor([
+        [1], [4], [7], [10]
+    ]) + 2 * torch.Tensor([
+        [2], [5], [8], [11]
+    ]) + 3 * torch.Tensor([
+        [3], [6], [9], [12]
+    ]) + 4 * torch.Tensor([
+        [13], [16], [19], [22]
+    ]) + 5 * torch.Tensor([
+        [14], [17], [20], [23]
+    ]) + 6 * torch.Tensor([
+        [15], [18], [21], [24]
+    ])
+
+    avar = Variable(a, requires_grad=True)
+    bvar = Variable(b, requires_grad=True)
+    cvar = Parameter(torch.Tensor([3]))
+    outvar = Kron()(avar, bvar, cvar).evaluate()
+    outvar.backward(gradient=gradient)
+
+    assert(torch.norm(avar.grad.data - actual_a) < 1e-6)
+    assert(torch.norm(bvar.grad.data - actual_b) < 1e-6)
+    assert(math.fabs(cvar.grad.data[0] - torch.trace(gradient)) < 1e-6)
 
