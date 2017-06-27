9c772b927a4ec934dbb2d585768fff4629a0a3e4
Geoff Pleiss <gpleiss@gmail.com>
2017-06-27 10:39:07 -0400

    Exact gp marginal log likeihood for kronecker matrices

--- gpytorch/math/functions/exact_gp_marginal_log_likelihood.py
+++ gpytorch/math/functions/exact_gp_marginal_log_likelihood.py
@@ -1,7 +1,10 @@
 import math
 import torch
 from torch.autograd import Variable, Function
+from gpytorch.lazy import KronVariable
+from gpytorch.utils.kron import kron_forward, kron_backward
 from .invmv import Invmv
+from .invmm import KronInvmm
 
 
 class ExactGPMarginalLogLikelihood(Invmv):
@@ -35,3 +38,74 @@ class ExactGPMarginalLogLikelihood(Invmv):
             y_grad = mat_inv_y.mul_(-grad_output_value)
 
         return mat_grad, y_grad
+
+
+class KronExactGPMarginalLogLikelihood(KronInvmm):
+    def forward(self, mat_a_eig_data, mat_b_eig_data, diag, y):
+        mat_a_evec = mat_a_eig_data[:, :-1]
+        mat_b_evec = mat_b_eig_data[:, :-1]
+        mat_a_eval = mat_a_eig_data[:, -1]
+        mat_b_eval = mat_b_eig_data[:, -1]
+        diag_val = diag.squeeze()[0]
+
+        m = mat_a_evec.size(0)
+        p = mat_b_evec.size(0)
+
+        # Caculate evec matrix
+        evec = kron_forward(mat_a_evec, mat_b_evec)
+
+        # Eval with noise
+        eval_with_noise = torch.ger(mat_a_eval, mat_b_eval).view(-1).add_(diag_val)
+
+        # Inverse quad
+        evec_eval_inv_prod = evec / eval_with_noise.unsqueeze(0).expand(m * p, m * p)
+        mat_inv_y = evec_eval_inv_prod.mv(evec.t().mv(y.view(-1)))
+        res = mat_inv_y.dot(y.view(-1))
+
+        # Log determinant
+        res += eval_with_noise.log().sum()
+
+        # Add constant and stuff
+        res += math.log(2 * math.pi) * len(y)
+        res *= -0.5
+
+        self.evec = evec
+        self.evec_eval_inv_prod = evec_eval_inv_prod
+        self.mat_inv_y = mat_inv_y
+        self.save_for_backward(y)
+        return y.new().resize_(1).fill_(res)
+
+
+    def backward(self, grad_output):
+        grad_output_value = grad_output.squeeze()[0]
+        mat_a_grad = None
+        mat_b_grad = None
+        diag_grad = None
+        y_grad = None
+
+        evec = self.evec
+        evec_eval_inv_prod = self.evec_eval_inv_prod
+        mat_inv_y = self.mat_inv_y
+        y, = self.saved_tensors
+
+        if any(self.needs_input_grad[:3]):
+            kron_grad = torch.ger(y.view(-1), mat_inv_y)
+            kron_grad.add_(-torch.eye(*kron_grad.size()))
+            kron_grad = torch.mm(evec_eval_inv_prod, evec.t().mm(kron_grad))
+            kron_grad.mul_(0.5 * grad_output_value)
+
+            mat_a_grad, mat_b_grad, diag_grad = kron_backward(
+                    self.mat_a,
+                    self.mat_b,
+                    kron_grad,
+                    self.needs_input_grad[:3]
+            )
+        
+        if self.needs_input_grad[3]:
+            y_grad = mat_inv_y.mul_(-grad_output_value)
+
+        return mat_a_grad, mat_b_grad, diag_grad, y_grad
+
+
+ExactGPMarginalLogLikelihood.register_lazy_function((KronVariable, Variable),
+        KronExactGPMarginalLogLikelihood)
--- test/math/functions/exact_gp_marginal_log_likelihood_test.py
+++ test/math/functions/exact_gp_marginal_log_likelihood_test.py
@@ -3,13 +3,21 @@ import torch
 import numpy as np
 from torch.autograd import Variable
 from gpytorch.math.functions import ExactGPMarginalLogLikelihood
+from gpytorch.lazy import Kron
+
 
 covar = torch.Tensor([
     [5, -3, 0],
     [-3, 5, 0],
     [0, 0, 2],
 ])
+other_covar = torch.Tensor([
+    [3, 0, -3],
+    [0, 2, 1],
+    [-3, 1, 6],
+])
 y = torch.randn(3)
+kron_y = torch.randn(9)
 
 
 def test_forward():
@@ -22,6 +30,22 @@ def test_forward():
     yvar = Variable(y)
     res = ExactGPMarginalLogLikelihood()(covarvar, yvar)
     assert(torch.norm(actual - res.data) < 1e-4)
+
+
+def test_forward_kron():
+    def evaluate_stub(self):
+        raise Exception('Evaluate should not be called')
+
+    covarvar = Kron()(Variable(covar), Variable(other_covar), Variable(torch.zeros(1)))
+    yvar = Variable(kron_y)
+    actual = ExactGPMarginalLogLikelihood()(covarvar.evaluate(), yvar)
+
+    covarvar = Kron()(Variable(covar), Variable(other_covar), Variable(torch.zeros(1)))
+    covarvar.evaluate = evaluate_stub.__get__(covarvar, covarvar.__class__)
+    yvar = Variable(kron_y)
+    res = ExactGPMarginalLogLikelihood()(covarvar, yvar)
+
+    assert(torch.norm(actual.data - res.data) < 1e-4)
 
 
 def test_backward():
@@ -42,3 +66,36 @@ def test_backward():
 
     assert(torch.norm(actual_mat_grad - covarvar.grad.data) < 1e-4)
     assert(torch.norm(actual_y_grad - yvar.grad.data) < 1e-4)
+
+
+def test_backward_kron():
+    covarvar = Variable(covar, requires_grad=True)
+    other_covarvar = Variable(other_covar, requires_grad=True)
+    diagvar = Variable(torch.zeros(1), requires_grad=True)
+    kronvar = Kron()(covarvar, other_covarvar, diagvar)
+    yvar = Variable(kron_y, requires_grad=True)
+    actual = ExactGPMarginalLogLikelihood()(kronvar.evaluate(), yvar)
+    actual.backward()
+
+    actual_covar_grad = covarvar.grad.data
+    actual_other_covar_grad = other_covarvar.grad.data
+    actual_diag_grad = diagvar.grad.data
+    actual_y_grad = yvar.grad.data
+
+    covarvar = Variable(covar, requires_grad=True)
+    other_covarvar = Variable(other_covar, requires_grad=True)
+    diagvar = Variable(torch.zeros(1), requires_grad=True)
+    kronvar = Kron()(covarvar, other_covarvar, diagvar)
+    yvar = Variable(kron_y, requires_grad=True)
+    res = ExactGPMarginalLogLikelihood()(kronvar, yvar)
+    res.backward()
+
+    res_covar_grad = covarvar.grad.data
+    res_other_covar_grad = other_covarvar.grad.data
+    res_diag_grad = diagvar.grad.data
+    res_y_grad = yvar.grad.data
+
+    assert(torch.norm(actual_covar_grad - res_covar_grad) < 1e-4)
+    assert(torch.norm(actual_other_covar_grad - res_other_covar_grad) < 1e-4)
+    assert(torch.norm(actual_diag_grad - res_diag_grad) < 1e-4)
+    assert(torch.norm(actual_y_grad - res_y_grad) < 1e-4)
